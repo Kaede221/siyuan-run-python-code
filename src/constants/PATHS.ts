a2c10c94aa52bb4776cb9656409e96e8be0e41b7
Kaede221 <kaedeshimizu@qq.com>
2026-03-13 16:35:53 +0800

    chore: 修改配置文件保存位置为 storage 目录

--- src/constants/PATHS.ts
+++ src/constants/PATHS.ts
@@ -2,4 +2,4 @@
  * 常用路径常量
  */
 
-export const CONFIG_FILE_PATH = "/data/widgets/siyuan_run_python_code_config.json";
+export const CONFIG_FILE_PATH = "/data/storage/siyuan_run_python_code_config.json";
